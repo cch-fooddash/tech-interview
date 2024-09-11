b3f9b6ba1a5ef5160d1dc71ec6a0ff81e6725a46
Lee Jungwoo <dlwjdd@naver.com>
2024-09-11 14:33:19 +0900

    에러 핸들링 오류 수정

--- src/shared/axios/index.ts
+++ src/shared/axios/index.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosError } from 'axios';
 import toast from 'react-hot-toast';
 
+type ErrorType = { title: string; code: string; detail: string };
 export const baseURL = 'https://user-api.dev.fooddash.co.kr';
 
 export const api = axios.create({
@@ -34,18 +35,24 @@ api.interceptors.response.use(
 
     return response;
   },
-  (error: AxiosError) => {
+  (error: AxiosError<ErrorType>) => {
     console.log(error.response?.status);
-    const data =
-      (error.response?.data as {
-        title: string;
-        code: string;
-        detail: string;
-      }) || undefined;
+    const errorStr = parseError(error);
 
-    toast.error(data?.detail || '오류가 발생 하였습니다.\n잠시 후 다시 시도 해주세요', {
+    toast.error(errorStr || '오류가 발생 하였습니다.\n잠시 후 다시 시도 해주세요', {
       duration: 5000,
     });
     return Promise.reject(error);
   }
 );
+
+export const parseError = (error: AxiosError<ErrorType>) => {
+  const serverError = error.response?.data.title || error.response?.data.detail || '';
+  const httpError = error.response?.statusText || '';
+  const axiosError = error.message || '';
+
+  if (serverError) return serverError;
+  if (httpError) return httpError;
+  if (axiosError) return axiosError;
+  return '에러가 발생했습니다 !';
+};
